b7d2769e70f5637386883150c32355a89b1edf20
shafiqurrehman <shafiq.12@live.com>
2017-11-26 21:39:47 +0500

    Analog Clock component commit

--- state-update-app/src/components/Analog.js
+++ state-update-app/src/components/Analog.js
@@ -1,8 +1,70 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 
-    export default function Analog(props){
-        let date = new Date(props.time)
+// export default function Analog(props){
+//     let date = new Date(props.time)
+//     let dialStyle = {
+//         position: 'relative',
+//         top: 0,
+//         left: 0,
+//         width: 200,
+//         height: 200,
+//         borderRadius: 20000,
+//         borderStyle: 'solid',
+//         borderColor: 'black'
+//     }
+
+//     let secondHandStyle = {
+//         position: 'relative',
+//         top: 100,
+//         left: 100,
+//         border: '1px solid red',
+//         width: '40%',
+//         height: 1,
+//         transform: 'rotate(' + ((date.getSeconds()/60)*360 - 90 ).toString() + 'deg)',
+//         transformOrigin: '0% 0%',
+//         backgroundColor: 'red'
+//         }
+
+//     let minuteHandStyle = {
+//         position: 'relative',
+//         top: 100,
+//         left: 100,
+//         border: '1px solid grey',
+//         width: '40%',
+//         height: 3,
+//         transform: 'rotate(' + ((date.getMinutes()/60)*360 - 90 ).toString() + 'deg)',
+//         transformOrigin: '0% 0%',
+//         backgroundColor: 'grey'
+//         }
+
+//     let hourHandStyle = {
+//         position: 'relative',
+//         top: 92,
+//         left: 106,
+//         border: '1px solid grey',
+//         width: '20%',
+//         height: 7,
+//         transform: 'rotate(' + ((date.getHours()/12)*360 - 90 ).toString() + 'deg)',
+//         transformOrigin: '0% 0%',
+//         backgroundColor: 'grey'
+//         }
+//         return(
+//             <div>
+//                 <div style={dialStyle}>
+//                 <div style={secondHandStyle}/>
+//                 <div style={minuteHandStyle}/>
+//                 <div style={hourHandStyle}/>
+//                 </div>
+//             </div>
+//         );
+//   }
+
+
+export default class Analog1 extends Component {
+    render() {
+
+        let date = new Date(this.props.time)
         let dialStyle = {
             position: 'relative',
             top: 0,
@@ -13,7 +75,7 @@ import React, {Component} from 'react';
             borderStyle: 'solid',
             borderColor: 'black'
         }
-      
+
         let secondHandStyle = {
             position: 'relative',
             top: 100,
@@ -21,11 +83,11 @@ import React, {Component} from 'react';
             border: '1px solid red',
             width: '40%',
             height: 1,
-            transform: 'rotate(' + ((date.getSeconds()/60)*360 - 90 ).toString() + 'deg)',
+            transform: 'rotate(' + ((date.getSeconds() / 60) * 360 - 90).toString() + 'deg)',
             transformOrigin: '0% 0%',
             backgroundColor: 'red'
-            }
-      
+        }
+
         let minuteHandStyle = {
             position: 'relative',
             top: 100,
@@ -33,11 +95,11 @@ import React, {Component} from 'react';
             border: '1px solid grey',
             width: '40%',
             height: 3,
-            transform: 'rotate(' + ((date.getMinutes()/60)*360 - 90 ).toString() + 'deg)',
+            transform: 'rotate(' + ((date.getMinutes() / 60) * 360 - 90).toString() + 'deg)',
             transformOrigin: '0% 0%',
             backgroundColor: 'grey'
-            }
-      
+        }
+
         let hourHandStyle = {
             position: 'relative',
             top: 92,
@@ -45,17 +107,19 @@ import React, {Component} from 'react';
             border: '1px solid grey',
             width: '20%',
             height: 7,
-            transform: 'rotate(' + ((date.getHours()/12)*360 - 90 ).toString() + 'deg)',
+            transform: 'rotate(' + ((date.getHours() / 12) * 360 - 90).toString() + 'deg)',
             transformOrigin: '0% 0%',
             backgroundColor: 'grey'
-            }
-            return(
-                <div>
-                    <div style={dialStyle}>
-                    <div style={secondHandStyle}/>
-                    <div style={minuteHandStyle}/>
-                    <div style={hourHandStyle}/>
-                    </div>
+        }
+        return (
+            <div>
+                <div style={dialStyle}>
+                    <div style={secondHandStyle} />
+                    <div style={minuteHandStyle} />
+                    <div style={hourHandStyle} />
                 </div>
-            );
-      }
+            </div>
+        );
+    }
+}
+
