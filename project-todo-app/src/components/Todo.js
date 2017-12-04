0b3494310dd9eac90c93db0d6cd7f127def306d4
shafiqurrehman <shafiq.12@live.com>
2017-12-04 13:17:07 +0500

    Project Todo app commit

--- project-todo-app/src/components/Todo.js
+++ project-todo-app/src/components/Todo.js
@@ -42,13 +42,14 @@ export default class Todo extends Component {
     render() {
         return(
             <div className="alignment">
+                <input type="text" placeholder="Enter todo list here" ref={(input) => this.todoInput = input} />
+                <button onClick={this.addTodo.bind(this)}>Add</button>
                 <ul>
                     { this.state.todos.map((todo, index) => {
                         return(<RemoveList id={index} key={index} todo={todo} onRemove={() => this.removeTodo(index)} />)
                     }) }
                 </ul>
-                <input type="text" placeholder="Enter todo list here" ref={(input) => this.todoInput = input} />
-                <button onClick={this.addTodo.bind(this)}>Add</button>
+                
           </div>
         );
     }
