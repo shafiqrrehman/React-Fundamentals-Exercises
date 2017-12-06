1c50805c7d2a5a12a55eb0c344ba4fd2189445e9
shafiqurrehman <shafiq.12@live.com>
2017-12-06 13:11:08 +0500

    Material UI first example commit

---
+++ material-ui-app/src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="Los Blancos"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+
+        <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderColumn>ID</TableHeaderColumn>
+                <TableHeaderColumn>Name</TableHeaderColumn>
+                <TableHeaderColumn>Status</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableRowColumn>1</TableRowColumn>
+                <TableRowColumn>John Smith</TableRowColumn>
+                <TableRowColumn>Employed</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>2</TableRowColumn>
+                <TableRowColumn>Randal White</TableRowColumn>
+                <TableRowColumn>Unemployed</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>3</TableRowColumn>
+                <TableRowColumn>Stephanie Sanders</TableRowColumn>
+                <TableRowColumn>Employed</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>4</TableRowColumn>
+                <TableRowColumn>Steve Brown</TableRowColumn>
+                <TableRowColumn>Employed</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>5</TableRowColumn>
+                <TableRowColumn>Christopher Nolan</TableRowColumn>
+                <TableRowColumn>Unemployed</TableRowColumn>
+              </TableRow>
+            </TableBody>
+          </Table>
+      </div>
+    );
+  }
+}
+
+export default App;
